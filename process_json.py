a542bff141fdca0dde51a71c1dca4cbedd2e8146
Brandon Roberts <brandon@bxroberts.org>
2024-10-09 15:36:35 -0700

    Make pipeline handle URL or path

--- process_json.py
+++ process_json.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 import os
 import json
+import hashlib
 import sys
 
 from PIL import Image, UnidentifiedImageError
+import requests
+
 from florence import run_florence
 
 
-ROOT_DIRECTORY = "/home/brandon/src/openmeasures-research-tools"
+DOWNLOAD_PATH = "/home/brandon/src/openmeasures-research-tools/media_telegram"
 
 INPUT_JSON=sys.argv[1]
 
 
-def log(msg):
-    sys.stderr.write(f"{msg}\n")
+def generate_file_hash(content):
+    return hashlib.sha256(content).hexdigest()
 
 
 def record_getter():
@@ -31,9 +34,10 @@ def record_getter():
 
 
 for record in record_getter():
-    try:
-        image_path = record["downloaded_media_path"]
-    except KeyError:
+    media_url_or_path = record.get(
+        "downloaded_media_path", record.get("media_url")
+    )
+    if not media_url_or_path:
         print(json.dumps(record))
         continue
 
@@ -42,29 +46,46 @@ for record in record_getter():
         print(json.dumps(record))
         continue
 
-    mediamimetype = record.get("mediamimetype")
+    print("media_url_or_path:", media_url_or_path, file=sys.stderr)
+
+    mediamimetype = record.get("mediamimetype", record.get("mime_type"))
     if not mediamimetype or not mediamimetype.startswith("image"):
+        print(json.dumps(record))
+        continue
+
+    # TODO: Parse the download media path, if it's an openmeasures file we have
+    # to download it sadly
+    if media_url_or_path.startswith("https://"):
+        print("Downloading", media_url_or_path, file=sys.stderr)
+        response = requests.get(media_url_or_path)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            hash = generate_file_hash(response.content)
+            attachment_path = os.path.join(DOWNLOAD_PATH, f"hash-{hash}.pdf")
+            if not os.path.exists(attachment_path):
+                print("Writing PDF to", attachment_path, file=sys.stderr)
+                with open(attachment_path, 'wb') as f:
+                    f.write(response.content)
+            media_url_or_path = attachment_path
+
+    if not os.path.exists(media_url_or_path):
+        print("File doesn't exist!", media_url_or_path, file=sys.stderr)
+        print(json.dumps(record))
+        continue
+
+    # see if we already did this within this JSON
+    if "media_information" in record:
         print(json.dumps(record))
         continue
 
     print(f"Running florence on media mime type {mediamimetype}...",
           file=sys.stderr)
 
-    joined_image_path = os.path.join(ROOT_DIRECTORY, image_path)
-
-    real_image_path = os.path.realpath(joined_image_path)
-    media_path, media_filename = os.path.split(real_image_path)
-    media_basename, _ = os.path.splitext(media_filename)
-    processed_media_path = os.path.join(media_path, f"{media_basename}-florence.json")
-    if os.path.exists(processed_media_path):
-        with open(processed_media_path, "r") as f:
-            record["media_information"] = json.load(f)
-        print(json.dumps(record))
-        continue
-
     try:
-        image = Image.open(joined_image_path)
-    except UnidentifiedImageError:
+        image = Image.open(media_url_or_path).convert("RGB")
+    except UnidentifiedImageError as e:
+        print(f"Error loading file {media_url_or_path}. Error: {e}",
+              file=sys.stderr)
         print(json.dumps(record))
         continue
 
@@ -72,7 +93,9 @@ for record in record_getter():
 
     prompt = "<CAPTION>"
     response = run_florence(image, prompt)
-    record["media_information"]["caption"] = response[prompt].strip()
+    caption = response[prompt].strip()
+    print("Caption", caption, file=sys.stderr)
+    record["media_information"]["caption"] = caption
 
     prompt = "<DENSE_REGION_CAPTION>"
     response = run_florence(image, prompt)
@@ -85,8 +108,5 @@ for record in record_getter():
     record["media_information"]["text"] = " ".join(
         response[prompt]["labels"]
     ).strip().replace("</s>", "")
-
-    with open(processed_media_path, "w") as f:
-        json.dump(record["media_information"], f)
 
     print(json.dumps(record))
